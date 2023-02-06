ad23b47ee72abc9f9dd0037c6c70314dda50b456
Artem <temazasport@mail.ru>
2023-02-06 16:45:59 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// Создать в html список состоящий из 3-х произвольных элементов li
+
+const buttonAdd = document.createElement('button');
+buttonAdd.textContent = 'добавить';
+buttonAdd.classList.add('add');
+
+const buttonDelete = document.createElement('button');
+buttonDelete.textContent = 'удалить';
+buttonDelete.classList.add('delete');
+
+const buttonToggle = document.createElement('button');
+buttonToggle.textContent = 'добавить/удалить класс';
+buttonToggle.classList.add('toggle');
+
+document.querySelector('body');
+document.body.append(buttonAdd);
+document.body.append(buttonDelete);
+document.body.append(buttonToggle);
+
+// Нужно создать кнопку которая будет добавлять элементы списка с текстом “новый элемент списка”
+
+const addNewElement = () => {
+    const li = document.createElement('li');
+    li.textContent = 'новый элемент списка';
+    document.querySelector('ul').appendChild(li);
+}
+buttonAdd.addEventListener('click', addNewElement);
+
+
+// Создать кнопку, которая будет удалять первый элемент из созданного списка
+
+const deleteEl = () => {
+    if (document.querySelector('li')) {
+        document.querySelector('li').remove();
+    }
+}
+buttonDelete.addEventListener('click', deleteEl);
+
+// Создать кнопку, при клике на которую ей добавляется-убирается класс “click”
+
+buttonToggle.addEventListener('click', (e) => {
+    e.target.classList.toggle('click');
+})
